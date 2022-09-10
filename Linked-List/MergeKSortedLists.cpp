5a868f8efe3f549ee224f5697e2bc788f3e58438
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-09-10 19:59:41 +0530

    Update MergeKSortedLists.cpp

--- Linked-List/MergeKSortedLists.cpp
+++ Linked-List/MergeKSortedLists.cpp
@@ -81,3 +81,84 @@ public:
         return ans;
     }
 };
+
+
+
+
+/*
+APPROCH: RECURSIVE APPROACH
+1. MERGE FIRST TWO LISTS IN ARRAY AND FIND ANS
+2. NOW ASK RECURSION TO MERGE REMAINING LISTS
+3. MERGE RECURSIONS ANS WITH OUR ANS
+*/
+class Solution {
+private: 
+    ListNode* solve(ListNode* h1, ListNode* h2){
+        ListNode* prev = h1;
+        ListNode* curr = h1->next;
+        ListNode* temp = h2;
+        while(temp != NULL && curr != NULL)
+        {
+            if(temp->val >= prev->val && temp->val <= curr->val)
+            {
+                prev->next = temp;
+                temp = temp->next;
+                prev->next->next = curr;
+                prev = prev->next;
+            }
+        else
+            {
+                prev = prev->next;
+                curr = curr->next;
+            }
+        }
+        
+        if(curr == NULL && temp != NULL)
+             prev->next = temp;
+        return h1;
+    }
+    
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) 
+    {
+        ListNode* h1 = list1;
+        ListNode* h2 = list2;
+        //check if either of list is empty 
+        if(h1 == NULL)
+            return h2;
+        if(h2 == NULL)
+            return h1;
+        ListNode* ans = NULL;
+        if(h1->val <= h2->val) 
+             ans = solve(h1,h2);
+        if(h1->val > h2->val)
+             ans = solve(h2,h1);
+        return ans;
+    }
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        // EDGE CASES
+        if(lists.size()==0){
+            return  NULL;
+        }
+        if(lists.size()==1){
+            return lists[0];
+        }
+        // MERGE FIRST TWO LISTS
+        ListNode* list1 = lists[0];
+        ListNode* list2 = lists[1];
+        ListNode* myAns = mergeTwoLists(list1,list2);
+        
+        // MERGE ANS WITH REMAINING LISTS USING RECURSION
+        vector<ListNode*> v1;
+        copy(lists.begin()+2, lists.end(), back_inserter(v1));
+        ListNode* recAns = mergeKLists(v1);
+        
+        // MERGE OUR ANS WITH RECURSIONS ANS
+        ListNode* ans = mergeTwoLists(myAns,recAns);
+        
+        //RETURN ANS
+        return ans;
+    }
+};
