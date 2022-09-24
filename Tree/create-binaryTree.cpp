e526062021a7d466df91f5950242201e7a0813ef
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-09-24 18:37:02 +0530

    Add files via upload

---
+++ Tree/create-binaryTree.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void createTree(Node *&root)
+{
+    cout << "Enter data : ";
+    int data;
+    cin >> data;
+    root = new Node(data);
+
+    if (data == -1)
+    {
+        return;
+    }
+    cout << endl
+         << "Insert in left of " << data << endl;
+    createTree(root->left);
+    cout << endl
+         << "Insert in right of " << data << endl;
+    createTree(root->right);
+}
+void preorder(Node *root)
+{
+    if (root->data == -1)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+void inorder(Node *root)
+{
+    if (root->data == -1)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+void postorder(Node *root)
+{
+    if (root->data == -1)
+    {
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+void leveOrderTraversal(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+        if (temp == NULL)
+        {
+            // 1 level has completely traversed
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left->data != -1)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right->data != -1)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+int main()
+{
+    //Tree data : 1 3 7 -1 -1 9 -1 -1 5 11 -1 -1 -1
+    Node *root = NULL;
+    createTree(root);
+    cout << endl << "Tree created succesfully" << endl;
+
+    cout<<endl;
+    cout << "PREORDER: ";
+    preorder(root);
+    cout << endl;
+
+    cout<<endl;
+    cout << "INORDER: ";
+    inorder(root);
+    cout << endl;
+
+    cout<<endl;
+    cout << "POSTORDER: ";
+    postorder(root);
+    cout << endl;
+
+    cout<<endl;
+    cout << "LEVELORDER: "<<endl;
+    leveOrderTraversal(root);
+    cout << endl;
+    return 0;
+}
