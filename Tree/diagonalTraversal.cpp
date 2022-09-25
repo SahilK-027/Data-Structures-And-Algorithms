388c23d3c702093760eef928820681d339ea2c97
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-09-25 21:44:28 +0530

    Update diagonalTraversal.cpp

--- Tree/diagonalTraversal.cpp
+++ Tree/diagonalTraversal.cpp
@@ -30,3 +30,38 @@ vector<int> diagonal(Node *root)
     }
     return res;
 }
+
+
+
+
+// Approach 2
+/* A binary tree node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+
+
+vector<int> diagonal(Node *root)
+{
+   vector<int> ans;
+   if(root == NULL){
+       return ans;
+   }
+   queue<Node*> q;
+   q.push(root);
+   
+   while(!q.empty()){
+       Node* temp = q.front();
+       q.pop();
+       while(temp != NULL){
+           if(temp->left){
+               q.push(temp->left);
+           }
+           ans.push_back(temp->data);
+           temp = temp->right;
+       }
+   }
+   return ans;
+}
