4acacc323915ed8cec775952094ee2fbd5e39bcd
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-10-05 13:41:59 +0530

    Update Flatten-Binary-Tree-to-Linked-List.cpp
    Added optimized Morris traversal code

--- Tree/Flatten-Binary-Tree-to-Linked-List.cpp
+++ Tree/Flatten-Binary-Tree-to-Linked-List.cpp
@@ -86,3 +86,37 @@ public:
         root = solve(root);
     }
 };
+
+
+
+
+// Approach 3: Using morris traversal
+// TC : O(N)
+// SC : O(1)
+class Solution {
+private:
+    TreeNode* findPredecessor(TreeNode* curr){
+        TreeNode* predecessor = curr->left;
+        while(predecessor->right != NULL){
+            predecessor = predecessor->right;
+        }
+        return predecessor;
+    }
+public:
+    void flatten(TreeNode* root) {
+        TreeNode* curr = root;
+        while(curr != NULL){
+            if(curr->left != NULL){
+                // left part exists
+                TreeNode* predecessor = findPredecessor(curr);
+                // Make link of predecessor with right part of tree
+                predecessor->right = curr->right;    
+                // Attach root->right to left
+                curr->right = curr->left;
+                // Make root left NULL
+                curr->left = NULL;
+            }
+            curr = curr->right;
+        }
+    }
+};
