f62ad9af350c39e38ca51cadff333d24bb0eb5c4
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-10-05 12:09:25 +0530

    Create Morris-Inorder-Traversal.cpp

---
+++ Tree/Morris-Inorder-Traversal.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+//Using Morris Traversal
+// TC: O(N)
+// SC: O(1)
+class Solution {
+private:
+    TreeNode* findPredecessor(TreeNode* curr){
+        TreeNode* temp = curr->left;
+        while(temp->right != NULL && temp->right != curr){
+            temp = temp->right;
+        }
+        return temp;
+    }
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root == NULL)
+        {
+            return ans;
+        }
+        // Start Traversal
+        TreeNode* curr = root;
+        while(curr != NULL){
+            if(!curr->left){
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+            else{
+                TreeNode* predecessor = findPredecessor(curr);
+                if(predecessor->right == NULL){
+                    // Create link
+                    predecessor->right = curr;
+                    curr = curr->left;
+                }
+                else{
+                    // Destroy link
+                    predecessor->right = NULL;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
+            }
+        }
+        return ans;
+    }
+};
