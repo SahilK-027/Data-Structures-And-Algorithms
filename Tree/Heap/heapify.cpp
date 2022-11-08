39bee0d02fa113a1e790f999257f57c8b934d521
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2022-11-08 11:49:05 +0530

    Update heapify.cpp

--- Tree/Heap/heapify.cpp
+++ Tree/Heap/heapify.cpp
@@ -1,38 +1,48 @@
 #include <bits/stdc++.h>
-using namespace std ;
-void heapify(int *arr, int n, int i){
-    // TIME COMPLEXITY : O(log n)
+using namespace std;
+void heapify(vector<int> &arr, int size, int index)
+{
+    int current = index;
+    int leftChild = index * 2 + 1;
+    int rightChild = index * 2 + 2;
 
-    // Take current find its right and left child
-    int current = i;
-    int left = 2*i;
-    int right = 2*i+1;
-
-    // If small found then swap
-    if(left < n && arr[current] < arr[left]){
-        swap(arr[current] , arr[left]);
+    // Check if current is smaller than left child
+    if (leftChild < size && arr[current] < arr[leftChild])
+    {
+        current = leftChild;
     }
 
-    if(right < n && arr[current] < arr[right]){
-        swap(arr[current] , arr[right]);
+    // Check if current is smaller than right child
+    if (rightChild < size && arr[current] < arr[rightChild])
+    {
+        current = rightChild;
     }
 
-    // if current got updated then check again for bottom subtree
-    if( current != i){
-        heapify(arr,n,current);
+    // If current is updated
+    if (current != index)
+    {
+        swap(arr[current], arr[index]);
+        heapify(arr, size, current);
     }
 }
 int main()
 {
-    int arr[6] = {-1,54,53,55,52,50};
-    int n = 5;
-    for(int i = n / 2 ; i > 0 ;i-- ){
-         heapify(arr, n, i);
-    }
-    for(int i = 1 ; i <= n ;i++)
+    vector<int> arr ;
+    arr.push_back(59);
+    arr.push_back(54);
+    arr.push_back(53);
+    arr.push_back(55);
+    arr.push_back(52);
+    arr.push_back(50);
+    int n = 6;
+    for (int i = n / 2 -1 ; i >= 0; i--)
     {
-        cout<<arr[i]<<' ';
+        heapify(arr, n, i);
     }
-    cout<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << ' ';
+    }
+    cout << endl;
     return 0;
 }
