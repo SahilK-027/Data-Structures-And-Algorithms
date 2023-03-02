400d73d24699a15a81512a3c4e5fe2c9cce34541
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2023-03-02 10:19:21 +0530

    Update recursion.cpp

--- DP/Fibonacci/recursion.cpp
+++ DP/Fibonacci/recursion.cpp
@@ -1,1 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+// Recusrive
+int fibo(int n){
+    if(n < 2) return n;
+    return fibo(n-1) + fibo(n-2);
+}
+int main()
+{
+    int n = 7;
+    // Recursive 
+    cout << fibo(n) << endl;
+    return 0;
+}
