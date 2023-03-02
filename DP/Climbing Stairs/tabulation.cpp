00076f4e6170bd2d2d5c8da1e3bf9019f960af65
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2023-03-02 10:35:08 +0530

    Create tabulation.cpp

---
+++ DP/Climbing Stairs/tabulation.cpp
@@ -0,0 +1,18 @@
+class Solution {
+private:
+    int solveTab(int n)
+    {
+        vector<int> dp(n+1);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+
+public:
+    int climbStairs(int n) {
+        return solveTab(n);
+    }
+};
