36b02fd68c37659335fa88d05b10c420f1e9d798
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2023-03-07 11:21:41 +0530

    Update recursion+memoization.cpp

--- DP/Painting Fence/recursion+memoization.cpp
+++ DP/Painting Fence/recursion+memoization.cpp
@@ -1,24 +1,21 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> 
 #define M 1000000007
-int add(int a,int b){
-    return ((a%M)+(b%M))%M;
-} 
+int solveMemoization(int n, int k, vector<int>& dp) {
+    // Base case
+    // We have only one pole
+    if(n == 1) return k ;
+    if(n == 2) return (k % M + (k % M  * 1ll * (k - 1) % M)) % M;
+    if(dp[n] != -1) return dp[n];
 
-int mult(int num1, int num2){
-    return ((num1%M) * 1LL * (num2%M))%M;
-}
-int solveMemoization(int n,int k,vector<int>&dp){
-    //base case
-    if(n==1)
-        return k;
-    if(n==2)
-        return add(k,mult(k,k-1));
-    if(dp[n]!=-1)
-        return dp[n];
-    dp[n]=add(mult(solveMemoization(n-2,k,dp),k-1),mult(solveMemoization(n-1,k,dp),k-1));
+    // Recursion
+    int last_same = (solveMemoization(n-2, k, dp) % M * 1ll * (k-1) % M) % M;
+    int last_diff = (solveMemoization(n-1, k, dp) % M * 1ll * (k-1) % M) % M;
+
+    return dp[n] = (last_same + last_diff) % M ;
 
 }
+
 int numberOfWays(int n, int k) {
-    vector<int>dp(n+1,-1);
-    return solveMemoization(n,k,dp);
+    vector<int> dp(n+1, -1);
+    return solveMemoization(n, k, dp);
 }
